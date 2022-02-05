ecb926a63b700eda1f0e87f1c1782a0a8c324255
Zsvob <svobodazachary@gmail.com>
2022-02-05 16:54:42 -0500

    server routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const router = require('express').Router();
+const db = require("../../db/db");
+const fs = require('fs');
+
+
+
+router.get("/api/notes", (req, res) => {
+    let results = db;
+    res.json(results);
+});
+
+router.post("/api/notes", (req, res) => {
+
+      // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote= {
+      title,
+      text,
+    };
+
+    // Obtain existing reviews
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in posting review');
+  }
+  
+});
+
+
+
+module.exports = router;
